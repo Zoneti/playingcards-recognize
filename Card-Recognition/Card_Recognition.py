06e982eea8936cf74b478ea18004d5d552044865
Gabriel Zoneti <gabriel.zoneti@gmail.com>
2019-06-05 20:40:26 -0300

    initial

---
+++ Card-Recognition/Card_Recognition.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+import cv2
+import os
+
+
+def load_image(dirname):
+    images = os.listdir(dirname)
+    for image in images:
+        img = cv2.imread(dirname+image)
+        resized_img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+        contours = find_contour_wrapper(resized_img)
+        
+        cards = get_cards_from_image(resized_img, contours)
+        highlight_detected(resized_img, cards, image)
+
+
+load_image('./testes/')
