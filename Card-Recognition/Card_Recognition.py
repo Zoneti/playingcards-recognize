ea89d7df549f4dc64f0f31320999b83627c059f9
Gabriel Zoneti <gabriel.zoneti@gmail.com>
2019-06-09 22:12:00 -0300

    Up proj

--- Card-Recognition/Card_Recognition.py
+++ Card-Recognition/Card_Recognition.py
@@ -2,17 +2,14 @@
 import numpy as np
 import cv2
 import os
+from contornos import encontre_contornos, realce_da_deteccao
+from reconhecimento_padroes import get_cards_from_image
 
+dir_teste_name = './testes/'
+imagem_teste_name = '2C.png'
 
-def load_image(dirname):
-    images = os.listdir(dirname)
-    for image in images:
-        img = cv2.imread(dirname+image)
-        resized_img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-        contours = find_contour_wrapper(resized_img)
-        
-        cards = get_cards_from_image(resized_img, contours)
-        highlight_detected(resized_img, cards, image)
-
-
-load_image('./testes/')
+img = cv2.imread(dir_teste_name+imagem_teste_name)
+image_redimensionada = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+contornos_encontrados = encontre_contornos(image_redimensionada)
+cartas_baralho = get_cards_from_image(image_redimensionada, contornos_encontrados)
+realce_da_deteccao(image_redimensionada, cartas_baralho, imagem_teste_name)
